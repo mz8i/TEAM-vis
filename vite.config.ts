52bfa7efc58f354fb67d72f2a9f4e88651a5ef75
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2023-03-21 14:39:22 +0100

    Simplify local testing on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [{ enforce: 'pre', ...mdx() }, react()],
+  plugins: [
+    { enforce: 'pre', ...mdx() },
+    react(),
+    qrcode({
+      filter: (url) => url.startsWith('http://192'),
+    }),
+  ],
   base: '/TEAM-vis/',
   server: {
     open: true,
+    host: '0.0.0.0',
   },
 });
